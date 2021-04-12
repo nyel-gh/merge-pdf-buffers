31ec604bcb20cc785419021984f66b669b3aaab1
Gabriel Vincent <gabriel@vincent.es>
2021-04-12 14:03:54 -0300

    Revert Prettier changes

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const hummus = require('hummus')
-const memoryStreams = require('memory-streams')
+const hummus = require('hummus');
+const memoryStreams = require('memory-streams');
 
 /**
- * Merges an array of PDF buffers into one single PDF buffer
- *
+ * Merges an array of PDF buffers into one single PDF buffer 
+ * 
  * @param {Buffer[]} buffers - a list of PDF buffers to be merged
- *
+ * 
  * @returns {Buffer} merged pdf buffer
  */
-exports.merge = function (buffers) {
-  const [first] = buffers
+exports.merge = async buffers => {
+  const [first] = buffers;
 
-  const outStream = new memoryStreams.WritableStream()
-  const firstPdfStream = new hummus.PDFRStreamForBuffer(first)
+  const outStream = new memoryStreams.WritableStream();
+  const firstPdfStream = new hummus.PDFRStreamForBuffer(first);
 
   const pdfWriter = hummus.createWriterToModify(
     firstPdfStream,
-    new hummus.PDFStreamForResponse(outStream)
-  )
+    new hummus.PDFStreamForResponse(outStream),
+  );
 
-  buffers.shift()
+  buffers.shift();
   buffers.forEach(buffer => {
-    const newPdfStream = new hummus.PDFRStreamForBuffer(buffer)
-    pdfWriter.appendPDFPagesFromPDF(newPdfStream)
-  })
+    const newPdfStream = new hummus.PDFRStreamForBuffer(buffer);
+    pdfWriter.appendPDFPagesFromPDF(newPdfStream);
+  });
 
-  pdfWriter.end()
-  return outStream.toBuffer()
-}
+  pdfWriter.end();
+  return outStream.toBuffer();
+};
